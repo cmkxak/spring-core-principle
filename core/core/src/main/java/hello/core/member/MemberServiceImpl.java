6e29fa1c8488021964ab1865f121b92646617729
cmkxak <gcm466@naver.com>
2022-03-30 01:55:51 +0900

    <수정> Component Scan 개념 적용

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,17 @@
 package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * 구현체가 하나만 있으면, Impl을 사용하는 것은 하나의 관례가 됨.
  */
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -18,5 +24,10 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findByID(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
     }
 }
--- core/core/src/main/java/hello/core/order/OrderServceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServceImpl.java
@@ -5,16 +5,19 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * 주문 결과 반환을 위해서는 Repo로 부터 회원 등급을 확인하고, 할인을 적용해야 함.
  */
-
+@Component
 public class OrderServceImpl implements OrderService{
     //final로 선언이 되있으면 생성자로 주입해줘야 함
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
@@ -28,6 +31,10 @@ public class OrderServceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrice); //할인 정책에 회원을 던짐
 
         return new Order(memberId, itemName, itemPrice, discountPrice); //새로운 주문 반환
-
     }
+    //테스트 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
+
 }
